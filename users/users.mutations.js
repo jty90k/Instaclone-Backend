147b3832b940ef76fadb1d8c13b20d076877152f
UniverseStark <jty90k@gmail.com>
2021-04-08 12:20:34 +0900

    4.1 Create Account part Two: 4.1 Create Account part Two 4.1 Create Account part Two

---
+++ users/users.mutations.js
@@ -0,0 +1,28 @@
+import client from "../client";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      // This code for Users @ Develpoer don't trust users just check everthings code
+      { firstName, lastName, username, email, password }
+    ) => {
+      // check if username or email are already on DB.
+      const existingUser = await client.user.findFirst({
+        where: {
+          OR: [
+            {
+              username,
+            },
+            {
+              email,
+            },
+          ],
+        },
+      });
+      console.log(existingUser);
+      // hash password
+      // save and return the user
+    },
+  },
+};
