fb38d16adf143b1d1377ddfa091af6292e6f9103
UniverseStark <jty90k@gmail.com>
2021-04-09 16:18:10 +0900

    4.9 authentication part Two

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ import schema from "./schema.js";
 const PORT = process.env.PORT;
 const server = new ApolloServer({
   schema,
+  context: {
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjE3OTUxNTM3fQ.0yyKOYvkvxc8efD-r_L-Hpb6ACkOEdcKZGCOhW4EnU0",
+  },
 });
 
 server
