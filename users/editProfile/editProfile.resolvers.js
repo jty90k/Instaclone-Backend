38547cf38f9e8c03d433c11e2fb439f1a4945634
UniverseStark <jty90k@gmail.com>
2021-04-09 19:35:02 +0900

    4.12 Protecting Resolvers

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,42 +1,44 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import client from "../../client.js";
+import { protectedResolver } from "../users.utils.js";
 
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, username, email, password: newPassword },
-      { loggedInUser, protectResolver }
-    ) => {
-      protectResolver(loggedInUser);
-      // hass passord
-      let uglyPassowrd = null;
-      if (newPassword) {
-        uglyPassowrd = await bcrypt.hash(newPassword, 10);
+    editProfile: protectedResolver(
+      async (
+        _,
+        { firstName, lastName, username, email, password: newPassword },
+        { loggedInUser, protectedResolver }
+      ) => {
+        protectedResolver(loggedInUser);
+        // hass passord
+        let uglyPassowrd = null;
+        if (newPassword) {
+          uglyPassowrd = await bcrypt.hash(newPassword, 10);
+        }
+        const updateUser = await client.user.update({
+          where: {
+            id: loggedInUser.id,
+          },
+          data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            ...(uglyPassowrd && { password: uglyPassowrd }),
+          },
+        });
+        if (updateUser) {
+          return {
+            ok: true,
+          };
+        } else {
+          return {
+            ok: false,
+            error: "Could not update user's update profile",
+          };
+        }
       }
-      const updateUser = await client.user.update({
-        where: {
-          id: loggedInUser.id,
-        },
-        data: {
-          firstName,
-          lastName,
-          username,
-          email,
-          ...(uglyPassowrd && { password: uglyPassowrd }),
-        },
-      });
-      if (updateUser) {
-        return {
-          ok: true,
-        };
-      } else {
-        return {
-          ok: false,
-          error: "Could not update user's update profile",
-        };
-      }
-    },
+    ),
   },
 };
