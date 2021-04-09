dd81660eef9046c2f4e09a8492d6e54ce92228b6
UniverseStark <jty90k@gmail.com>
2021-04-09 15:11:40 +0900

    4.7 updateProfile

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,39 @@
+import bcrypt from "bcrypt";
+import client from "../../client.js";
+
 export default {
   Mutation: {
-    editProfile: () => console.log("hi"),
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password: newPassword }
+    ) => {
+      let uglyPassowrd = null;
+      // hass passord
+      if (newPassword) {
+        uglyPassowrd = await bcrypt.hash(newPassword, 10);
+      }
+      const updateUser = await client.user.update({
+        where: {
+          id: 1,
+        },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(uglyPassowrd && { password: uglyPassowrd }),
+        },
+      });
+      if (updateUser) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: "Could not update user's update profile",
+        };
+      }
+    },
   },
 };
