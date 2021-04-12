8f8a3e26cf29135b9684de982f75d8c8e4e7eac5
UniverseStark <jty90k@gmail.com>
2021-04-12 12:53:57 +0900

    test1

---
+++ server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+import express from "express";
+import logger from "morgan";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs, resolvers } from "./schema.js";
+import { getUser, protectResolver } from "./users/users.utils.js";
+
+const PORT = process.env.PORT;
+const apollo = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => {
+    return {
+      loggedInUser: await getUser(req.headers.token),
+      protectResolver,
+    };
+  },
+});
+
+const app = express();
+apollo.applyMiddleware({ app });
+app.use(logger("tiny"));
+app.use("/static", express.static("uploads"));
+app.listen({ port: PORT }, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT} ✅`);
+});
---
+++ users/editProfile/editProfile.resolvers.js
@@ -0,0 +1,58 @@
+import { createWriteStream } from "fs";
+import bcrypt from "bcrypt";
+import client from "../../client.js";
+import { protectedResolver } from "../users.utils.js";
+
+console.log(process.cwd());
+
+const resolverFn = async (
+  _,
+  { firstName, lastName, username, email, password: newPassword, bio, avatar },
+  { loggedInUser }
+) => {
+  let avatarUrl = null;
+  if (avatar) {
+    const { filename, createReadStream } = await avatar;
+    const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
+    const readStream = createReadStream();
+    const writeStream = createWriteStream(
+      process.cwd() + "/uploads/" + newFilename
+    );
+    readStream.pipe(writeStream);
+    avatarUrl = `http://localhost:4000/static/${newFilename}`;
+  }
+  // hass passord
+  let uglyPassowrd = null;
+  if (newPassword) {
+    uglyPassowrd = await bcrypt.hash(newPassword, 10);
+  }
+  const updateUser = await client.user.update({
+    where: {
+      id: loggedInUser.id,
+    },
+    data: {
+      firstName,
+      lastName,
+      username,
+      email,
+      bio,
+      ...(uglyPassowrd && { password: uglyPassowrd }),
+      ...(avatarUrl && { avatar: avatarUrl }),
+    },
+  });
+  if (updateUser) {
+    return {
+      ok: true,
+    };
+  } else {
+    return {
+      ok: false,
+      error: "Could not update user's update profile",
+    };
+  }
+};
+export default {
+  Mutation: {
+    editProfile: protectedResolver(resolverFn),
+  },
+};
