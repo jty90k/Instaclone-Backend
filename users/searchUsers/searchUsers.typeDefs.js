9dbc6285f7519b8af8f4a904662cf748987d8c73
UniverseStark <jty90k@gmail.com>
2021-04-13 18:16:40 +0900

    6.0 Photos Model

--- users/searchUsers/searchUsers.typeDefs.js
+++ users/searchUsers/searchUsers.typeDefs.js
@@ -2,6 +2,7 @@ import { gql } from "apollo-server-core";
 
 // error를 띄우고 싶다면 여기에서 하면 된다.
 // keyword를 3~4개 글자보다 길게 입력하라고 메세지 띄울 수 있다.
+// login인 된 유저나 안된 유저 상관없이 검색 가능
 export default gql`
   type Query {
     searchUsers(keyword: String!): [User]
