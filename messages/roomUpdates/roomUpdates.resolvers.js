470b31a2d97dcce02650905391329d415bdbf13e
UniverseStark <jty90k@gmail.com>
2021-04-17 19:08:36 +0900

    7.11 Authenticating Subscriptions

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -6,12 +6,19 @@ import pubsub from "../../pubsub.js";
 export default {
   Subscription: {
     roomUpdates: {
+      // server.js -> here /
       subscribe: async (root, args, context, info) => {
         //room이 존재하는지 안 하는지 찾는 로직
         //존재한다면, 유저는 업데이트를 리스닝할 수 있는 거야.
-        const room = await client.room.findUnique({
+        //user가 리스닝하기 이전에 체크하는 거거든
+        const room = await client.room.findFirst({
           where: {
             id: args.id,
+            users: {
+              some: {
+                id: context.loggedInUser.id,
+              },
+            },
           },
           select: {
             id: true,
@@ -21,10 +28,33 @@ export default {
         if (!room) {
           throw new Error("You shall not see this");
         }
+        //이 witherFilter의 두번째 function은 우리가 publish할 때만 실행 돼.
         return withFilter(
+          //user가 리스닝을 시작하기 전에 체크하는데, 만약 여러분이 다시 체크하기 원한다면 그렇게도 할 수도 있지.
           () => pubsub.asyncIterator(NEW_MESSAGE),
-          ({ roomUpdates }, { id }) => {
-            return roomUpdates.roomId === id;
+          //(중요) roomUpdates 이 function은 모든 trigger, 즉 모든 publish에서 사용될 거야.
+          // arguments=roomUpdates , conext=id
+          //roomUpdates from sendMessage.resolvers.js (pubsub.publish~)
+          async ({ roomUpdates }, { id }, { loggedInUser }) => {
+            if (roomUpdates.roomId === id) {
+              const room = await client.room.findFirst({
+                where: {
+                  id,
+                  users: {
+                    some: {
+                      id: loggedInUser.id,
+                    },
+                  },
+                },
+                select: {
+                  id: true,
+                },
+              });
+              if (!room) {
+                return false;
+              }
+              return true;
+            }
           }
         )(root, args, context, info);
       },
--- server.js
+++ server.js
@@ -10,12 +10,34 @@ const PORT = process.env.PORT;
 const apollo = new ApolloServer({
   typeDefs,
   resolvers,
-  context: async ({ req }) => {
-    if (req) {
+  context: async (ctx) => {
+    if (ctx.req) {
       return {
-        loggedInUser: await getUser(req.headers.token),
+        loggedInUser: await getUser(ctx.req.headers.token),
+      };
+    } else {
+      // 지금은 그냥 context를 우리의 필요에 맞게 뽑아낸 거야.
+      const {
+        connection: { context },
+      } = ctx;
+      return {
+        loggedInUser: context.loggedInUser,
       };
     }
+  },
+  //connectionParams는 기본적으로 우리가 볼 수 있는 HTTP headers야!
+  //onConnect에 기반해서 user를 인증 -> onConnect는 connection이 이뤄지는 순간에 우리에게 HTTP headers를 준다.
+  //우리가 onConnect에서 무엇을 return하던지 context의 한 부분이 될거야.
+  subscriptions: {
+    onConnect: async ({ token }) => {
+      if (!token) {
+        throw new Error("You can't listent.");
+      }
+      const loggedInUser = await getUser(token);
+      return {
+        loggedInUser,
+      };
+    },
   },
 });
 
