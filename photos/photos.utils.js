82827f2d61b8236e382782ebf18ab5c1f1808fdb
UniverseStark <jty90k@gmail.com>
2021-05-14 19:08:45 +0900

    Instaclone-web11.17 Create Comment part Two(important)

--- photos/photos.utils.js
+++ photos/photos.utils.js
@@ -1,4 +1,6 @@
 //hashtags.map으로 만든 배열을 리턴한다.
+//match를 이용한 hastags 찾는 방법
+
 export const processHashtags = (caption) => {
   const hashtags = caption.match(/#[\w]+/g) || [];
   console.log(hashtags);
